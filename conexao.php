6d31883220d7efe74bd522b5fc985292f356f5de
TaigcoutoProf <taigcouto.prof@gmail.com>
2025-07-22 11:35:23 -0300

    Primeiro commit: arquivos iniciais

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
-$hostname = "localhost";
-$usuario = "root";
-$senha = "";
+$hostname = "sql100.infinityfree.com";
+$usuario = "if0_39533697";
+$senha = "dz5BSKCKIvXhIUy";
 $bancodedados = "banco_php";
 
 $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
